59d7ab2ad90fd001aeaacc9df02f46d554828e9a
Sam <samtyler6@gmail.com>
2020-01-19 14:50:59 +0000

    Support find_package(fast_ber)

--- cmake/fast_ber_generate.cmake
+++ cmake/fast_ber_generate.cmake
@@ -2,7 +2,7 @@ function(fast_ber_generate input_file output_name)
     file (MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/autogen)
     add_custom_command(
         OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/autogen/${output_name}.hpp
-        COMMAND ${CONAN_BIN_DIRS_FAST_BER}/fast_ber_compiler ${input_file} ${CMAKE_CURRENT_BINARY_DIR}/autogen/${output_name}
-        DEPENDS ${CONAN_BIN_DIRS_FAST_BER}/fast_ber_compiler ${input_file}
+        COMMAND fast_ber::fast_ber_compiler ${input_file} ${CMAKE_CURRENT_BINARY_DIR}/autogen/${output_name}
+        DEPENDS fast_ber::fast_ber_compiler ${input_file}
     )
 endfunction(fast_ber_generate)
