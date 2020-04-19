a463b200d5f38037b04abecdf5c3813088f503b1
Sam <samtyler6@gmail.com>
2020-04-19 16:45:54 +0100

    Add build_modules to conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -8,11 +8,11 @@ class FastberConan(ConanFile):
     author = "Samuel Tyler"
     url = "https://github.com/Samuel-Tyler/fast_ber"
     description = "A performant ASN.1 BER encoding and decoding library written in C++11"
-    topics = ("ASN", "ASN1", "ASN.1", "BER", "Serialization")
+    topics = "ASN", "ASN1", "ASN.1", "BER", "Serialization"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = {"shared": False}
-    generators = ("cmake", "cmake_find_package_multi")
+    generators = "cmake", "cmake_find_package_multi"
     requires = "abseil/20200205"
     scm = {"type": "git", "url": url, "revision": "master", "subfolder": name}
 
@@ -29,5 +29,8 @@ class FastberConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.names["cmake_find_package"] = "fast_ber"
+        self.cpp_info.names["cmake_find_package_multi"] = "fast_ber"
+        self.cpp_info.build_modules.append("lib/fast_ber/fast_ber_generate.cmake")
         self.env_info.PATH = os.path.join(self.package_folder, "bin")
 
